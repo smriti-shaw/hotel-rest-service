9d7baa7c1ede0c247b0c8556423148745279d567
smriti shaw <smriti.smriti13@gmail.com>
2019-04-10 12:14:17 +0530

    add repo , controller and dtos

--- hotel/src/main/java/com/learnthetek/hotel/HotelApplication.java
+++ hotel/src/main/java/com/learnthetek/hotel/HotelApplication.java
@@ -2,10 +2,11 @@ package com.learnthetek.hotel;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @EnableScheduling
-@SpringBootApplication(scanBasePackages = { "com.learnthetek.hotel"})
+@SpringBootApplication(scanBasePackages = {"com.learnthetek.hotel"})
 public class HotelApplication {
 
 	public static void main(String[] args) {
